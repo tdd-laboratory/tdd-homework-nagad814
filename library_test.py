762aab74443c9bba48d7c77875836e37b35df157
Naga durga <naga.durga@evernym.com>
2018-06-21 15:38:03 +0530

    finished test

--- library_test.py
+++ library_test.py
@@ -30,6 +30,55 @@ class TestCase(unittest.TestCase):
     def test_no_integers(self):
         self.assert_extract("no integers", library.integers)
 
+    # Fourth unit test; 
+    def test_dates(self):
+        self.assert_extract("I was born on 2015-07-25.", library.dates_iso8601, '2015-07-25')
+    
+    # Fifth unit test; 
+    def test_wrong_date(self):
+        self.assert_extract("is this date correct 2015-13-25.", library.dates_iso8601)
+
+    # Sixth unit test; 
+    def test_human_dates(self):
+        self.assert_extract("I was born on 25 Jan 2017.", library.dates_human, '25 Jan 2017')
+
+    def match_complex_dates(self):
+        ''' dates with time stamps with minute '''
+        self.assert_extract("I was born on 2008-09-15T15:53.", library.complex_date_time, '2008-09-15T15:53')
+
+        ''' dates with time stamp with seconds'''
+        self.assert_extract("I was born on 2008-09-15T15:53:00.", library.complex_date_time, '2008-09-15T15:53:00')
+
+        ''' dates with time stamp with milliseconds '''
+        self.assert_extract("I was born on 2008-09-15T15:53:00.322345.", library.complex_date_time, '2008-09-15T15:53:00.322345')
+
+        ''' date time seperated with space '''
+        self.assert_extract("I was born on 2008-09-15 15:53:00.", library.complex_date_time, '2008-09-15 15:53:00')
+
+        ''' date time seperated with T '''
+        self.assert_extract("I was born on 2008-09-15T15:53:00.", library.complex_date_time, '2008-09-15T15:53:00')
+
+        ''' with three letter Timezone '''
+        self.assert_extract("I was born on 2008-09-15T15:53:00 EST.", library.complex_date_time, '2008-09-15T15:53:00 ES')
+
+        ''' with one letter Timezone '''
+        self.assert_extract("I was born on 2008-09-15T15:53:00Z.", library.complex_date_time, '2008-09-15T15:53:00Z')
+
+        ''' with Time offset '''
+        self.assert_extract("I was born on 2008-09-15T15:53:00 -0800.", library.complex_date_time, '2008-09-15T15:53:00 -0800')
+
+        ''' with time stamp with milliseconds and Time offset and space'''
+        self.assert_extract("I was born on 2008-09-15 15:53:00.345623 -0800.", library.complex_date_time, '2008-09-15 15:53:00.345623 -0800')
+
+        ''' with time stamp with seconds and three letter Timezone and T'''
+        self.assert_extract("I was born on 2008-09-15T15:53:00 EST.", library.complex_date_time, '2008-09-15T15:53:00 EST')
+    
+    def match_grouped_numbers(self):
+        self.assert_extract("pay me 123,456,789.", library.grouped_number, '123,456,789')
+        self.assert_extract("pay me 12,34,56,789.", library.grouped_number, '12,34,56,789')
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
